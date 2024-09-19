a95e8d95cde4baa326ea6ba54af1369be48e8aa1
tinojona <wind.tino@gmail.com>
2024-09-19 11:44:07 +0200

    aggregate hosp by the station (regions depending on the 4 buffers) include foehn and temp and assign it directly to the right station so that it is saved together

--- analysis/processing_hosp.R
+++ analysis/processing_hosp.R
@@ -14,12 +14,18 @@ rm(list = ls())
 data = read.csv("C:/Users/tinos/Documents/Master - Climate Science/3 - Master Thesis/master_thesis/data-raw/MedStat/med_stat_data/hosp_daily.csv", header =T)
 data$station = NA
 
+
 # get regions sorted to station files
 files_regions = list.files("C:/Users/tinos/Documents/Master - Climate Science/3 - Master Thesis/master_thesis/data/MetStatRegions")[c(2,4,7,10)]
 
 # recreate all the buffer sies that are in files_regions, in THE SAME ORDER
 buffer_sizes = c(1000,10000,2500,5000)
 
+# get foehn data for every station
+files_foehn = list.files("C:/Users/tinos/Documents/Master - Climate Science/3 - Master Thesis/master_thesis/data/foehn_processed")
+
+# get temperature data for every station
+files_temp = list.files("C:/Users/tinos/Documents/Master - Climate Science/3 - Master Thesis/master_thesis/data/temp_processed")
 
 # start loop for 4 buffers
 for(i in 1:4){
@@ -31,19 +37,41 @@ for(i in 1:4){
   # get colnames of the stations for later
   station_names = colnames(regions)
 
+  # create empty aggregated_by_buffer data.frame for later here
+  aggregated_by_buffer = data.frame(DT_EINTRTTSDAT = character(),
+                                    all = numeric(),
+                                    a014 = numeric(),
+                                    a1564y = numeric(),
+                                    a6574y = numeric(),
+                                    a7584y = numeric(),
+                                    a85plusy = numeric(),
+                                    mal = numeric(),
+                                    fem = numeric(),
+                                    inf = numeric(),
+                                    ment = numeric(),
+                                    cvd = numeric(),
+                                    resp = numeric(),
+                                    uri = numeric(),
+                                    station = character())
+
   # start loop for different station names
   for (j in 1:8) {
 
+    # to clear the index for later
+    data$station = NA
+
     # current station name
     station_current = station_names[j]
+    # print(station_current) # for code checking
 
-    # get all region names of that station
+    # get all region names of that station, exclude empty entries
     region_names = regions[regions[,j] != "",j ]
+    # print(region_names) # for code checking
 
     # assign the station name to data$station when one of the region names is present
     # first create index
     index <- sapply(data$ID_WOHNREGION, function(row_text) {
-      any(sapply( region_names,function(word) grepl(word, row_text)))
+      any(sapply( region_names,function(word) grepl(word, row_text, ignore.case = TRUE)))
     })
 
     data$station[index] = station_current
@@ -56,17 +84,43 @@ for(i in 1:4){
       summarise(across(all:uri, sum),         # across al variables we sum
                 station = station_current)    # but we keep the station name
 
-    # add temperature and foehn data to the aggregated_by_station
+    print(nrow(aggregated_by_station)) #delete later
+
+    # rename date column so that it matches with foehn and temp later
+    aggregated_by_station <- aggregated_by_station %>%
+      rename("date" = DT_EINTRITTSDAT)
+
+    # cbind the foehn data to the appropriate station
+    foehn_data = read.csv(paste0("C:/Users/tinos/Documents/Master - Climate Science/3 - Master Thesis/master_thesis/data/foehn_processed/", files_foehn[j]))
+    foehn_data <- foehn_data %>%
+      rename("date" = time_conv)
+
+    aggregated_by_station <- aggregated_by_station %>%
+      left_join(foehn_data[,2:3], by = "date")
 
 
-    # save the data
+    # cbind the temp data
+    temp_data = read.csv(paste0("C:/Users/tinos/Documents/Master - Climate Science/3 - Master Thesis/master_thesis/data/temp_processed/", files_temp[j]))
+    temp_data$date <- as.character(as.Date(strptime(as.character(temp_data$time), format = "%Y%m%d")))
+
+    aggregated_by_station <- aggregated_by_station %>%
+      left_join(temp_data[,3:4], by = "date")
+
+    print(nrow(aggregated_by_station)) #delete later
+
+
+    # save the data per station and buffer
     file_name = paste0(station_current, "_buffer_", buffer, ".csv")
     write.csv(aggregated_by_station, file = paste0("C:/Users/tinos/Documents/Master - Climate Science/3 - Master Thesis/master_thesis/data/MedStat_aggregated/by_station/", file_name))
 
+
+    # append the data to the buffer aggregated dataset
+    aggregated_by_buffer = rbind(aggregated_by_buffer, aggregated_by_station)
+
   }
 
-  # save the data of one buffer in one data.frame
-  # aggregated_by_buffer = rbind(aggregated_by_buffer ,aggregated_by_station)
+  # save the combined buffer set
+  write.csv(aggregated_by_buffer, file = paste0("C:/Users/tinos/Documents/Master - Climate Science/3 - Master Thesis/master_thesis/data/MedStat_aggregated/hosp_buffer_", buffer, ".csv"))
 
 }
 
