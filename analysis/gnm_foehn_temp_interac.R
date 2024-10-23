f9eb1b29ffc3ae36a6e101d4d8871a671cd66d96
tinojona <wind.tino@gmail.com>
2024-10-23 16:54:20 +0200

    change comparison from foehn and all to foehn and no foehn

--- analysis/gnm_foehn_temp_interac.R
+++ analysis/gnm_foehn_temp_interac.R
@@ -1,6 +1,6 @@
 ################################################################################
 # GNM FOEHN as a modifier of temperature
-# - the lower the binary threshold the better the model performance
+# - the lower the binary threshold (0 is best) the better the model performance
 
 # notes:
 
@@ -57,7 +57,7 @@ arglag = list(fun="integer")
 #####
 
 
-bin_thresholds = c(5,10,20,40,80,120,160, 180 ,200,240,270)
+bin_thresholds = c(0,5)#,10,20,40,80,120,160, 180 ,200,240,270)
 
 ### ALGORITHM ####
 
@@ -111,39 +111,47 @@ View(qaic_tab)
 
 ### VISUALIZATION of the best performing combination ####
 par(mfrow=c(2,2))
-for( i in c(5,60,120,160)){
+for( i in c(10,35,80,140)){ #c(0,5,60,120
 
 # binary foehn
 foehn_bin <- ifelse(data$f_id > i, 0, 1) #  as.numeric(opt_var) c(20,40,80,120,160,200,240,270)
+foehn_bin_rev <- ifelse(foehn_bin == 1, 0, 1)
+
 
 modif <- cb.temp * foehn_bin
+modif_rev <- cb.temp *foehn_bin_rev
 
 # model with and without foehn
-mod            <- gnm(all ~  cb.temp, data = data,  family=quasipoisson(), eliminate=stratum_dow, subset=ind_dow>0)
-mod_w_modifier <- gnm(all ~  cb.temp + modif, data = data,  family=quasipoisson(), eliminate=stratum_dow, subset=ind_dow>0)
+mod_modif <- gnm(all ~  cb.temp + modif, data = data,  family=quasipoisson(), eliminate=stratum_dow, subset=ind_dow>0)
+mod_modif_rev <- gnm(all ~  cb.temp + modif_rev, data = data,  family=quasipoisson(), eliminate=stratum_dow, subset=ind_dow>0)
+
+# return significance of modif term in model
+# coef_summary <- summary(mod_modif)$coefficients
+# p_value_mod_modif <- coef_summary["mod_modif", "Pr(>|z|)"]
+# p_value_mod_modif
+
 
 # prediction with and without foehn
-pred <- crosspred(cb.temp, mod, cen = 28, cumul=FALSE)
-pred_w_modifier <- crosspred(cb.temp, mod_w_modifier, cen = 28, cumul=FALSE)
+pred_modif <- crosspred(cb.temp, mod_modif, cen = 20, cumul=FALSE)
+pred_modif_rev <- crosspred(cb.temp, mod_modif_rev, cen = 20, cumul=FALSE)
 
 
 
-
-plot(pred,              ## cumulative exposure
+plot(pred_modif,              ## cumulative exposure
      "overall",
      col = 2,
      ci.arg = list(density = 10, col = 2 ,angle = -45),
      lwd = 2,
-     main = paste0("Overall cum exp-resp: modifier binary thr=",i))
+     main = paste0("Overall cum exp-resp: modifier, binary thr=",i))
 
-lines(pred_w_modifier,           ## cumulative exposure
+lines(pred_modif_rev,           ## cumulative exposure
      "overall",
      col = 4,
      ci = "area",
      ci.arg = list(density = 10, col = 4 ,angle = 45),
      lwd = 2)
 
-legend("topright", legend = c("temp", "temp+modifier foehn"), col = c(2,4), lwd = 2)
+legend("topright", legend = c("temp+modif", "temp-modif"), col = c(2,4), lwd = 2)
 }
 #####
 
